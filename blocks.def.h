03518dde8050f06360946509f4e37be8eaeff34f
Saverio Turetta <saverioturetta@protonmail.com>
2020-09-16 22:23:01 +0300

    edited blocks.def.h and Makefile

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+        /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
+        {" ", "keyboard-layout",       0,              11},
+        {"", "internet",               5,              0},
+        {"", "battery",               20,             0},
+        {"", "volume",                 0,              10},
+        {"", "disk /dev/elxnoname-vg/root /",  60,             0},
+        {"", "cpu",                    10,             0},
+        {"", "memory",                 20,             0},
+        {"", "clock",                50,             0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
